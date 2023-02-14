86240cfe6ad9a86cd3e314a1b6f72354016cd164
shaghayegh momeni <shghygh.momeni18@gmail.com>
2023-02-14 11:25:35 +0330

    write actions for reducer

---
+++ src/store/actions/actions.js
@@ -0,0 +1,9 @@
+//Actions
+
+export const ReadAllExpenses = (expenses) => {
+  return { type: "READ_EXPENSES", expenses };
+};
+
+export const AddExpense = (newExpense) => {
+  return { type: "ADD_EXPENSE", newExpense };
+};
