511a4b2f2bddc00c01ed5513a475f70f5b69e9d3
shaghayegh momeni <shghygh.momeni18@gmail.com>
2023-02-14 11:33:16 +0330

    write reducer switch case

--- src/store/reducer/index.jsx
+++ src/store/reducer/index.jsx
@@ -1,37 +1,15 @@
-export const ExpensesReducer = async (state, action) => {
+const ExpensesReducer = (state, action) => {
   switch (action.type) {
-    case "GET": {
-      try {
-        const response = await axios.get(
-          "https://63e7b432cbdc5658737be69b.mockapi.io/expenses"
-        );
-        return response.data;
-      } catch (err) {
-        const errorMessage = "Error: " + err.message;
-        console.log(errorMessage);
-      }
-      // let res = await axios.get(
-      //   "https://63e7b432cbdc5658737be69b.mockapi.io/expenses"
-      // );
-      // let { data } = res.data;
-      // console.log("Show Result", data);
-    }
-
-    case "CREATE":
-      return [
-        ...state,
-        {
-          title: action.payload.title,
-          cost: action.payload.cost,
-          id: Date.now(),
-        },
-      ];
-
-    case "DELETE": {
-      return state.filter((t) => t.id !== action.payload.id);
-    }
-
+    case "READ_EXPENSES":
+      return action.payload;
+    case "ADD_EXPENSE":
+      return [...state, action.payload];
     default:
       return state;
   }
 };
+export default ExpensesReducer;
+
+// case "DELETE": {
+//   return state.filter((t) => t.id !== action.payload.id);
+// }
