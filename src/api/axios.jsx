453fd562a4db75cd8c59936fcc2521e4147d9d35
shaghayegh momeni <shghygh.momeni18@gmail.com>
2023-02-14 10:14:39 +0330

    write api url

--- src/api/axios.jsx
+++ src/api/axios.jsx
@@ -1,5 +1,4 @@
 import axios from "axios";
-export const BASE_URL = "https://63e7b432cbdc5658737be69b.mockapi.io/expenses";
 
 const fetchExpenses = () => axios.get();
 
---
+++ src/config/linksAPI.js
@@ -0,0 +1,1 @@
+export const BASE_URL = "https://63e7b432cbdc5658737be69b.mockapi.io/expenses";
